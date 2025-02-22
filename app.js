4a186c606a1ea3807a272caf3cf750e707dbbaf4
MakhmudSD <makhwork15@gmail.com>
2025-02-22 21:36:12 +0900

    refactor modification

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("Let's start the project");
+const express = require('express');
+const res = require("express/lib/response");
+const app = express();
+
+const db = express("/.server").db()
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.post("/create-item", (req, res) => {
+    // TODO Db code here!!!
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+    res.render("reja");
+});
+
+modules_export = app;
