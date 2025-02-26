35d8b57994301f59bc8c49b47caab6eabb4f9c39
MakhmudSD <makhwork15@gmail.com>
2025-02-26 10:25:17 +0900

    Plans Delete Completed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 // MongoDB connected
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -19,6 +20,15 @@ app.post("/create-item", (req, res) => {
     db.collection("plans").insertOne({ reja : new_reja}, (err, data) => {
         res.json(data.ops[0]);
     })
+});
+
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans")
+    .deleteOne({_id: new mongodb.ObjectId(id)}, 
+    function (err, data) {
+            res.json({ state: "success" });
+        });   
 });
 
 app.get("/author", (req, res) => {
--- public/browser.js
+++ public/browser.js
@@ -3,10 +3,10 @@ console.log("FrontEnd JS Executed");
 function itemTemplate(item) {
   return `<li
             class="list-group-item list-group-item-info d-flex align-items-center justify-content-between border border-1">
-            <span class="item-text"><${item.reja}></span>
+            <span class="item-text">${item.reja}</span>
             <div>
-                <button data-id="<${item._id}>" class="edit-me btn btn-success btn-sm mr-1 me-md-2">Edit</button>
-                <button data-id="<${item._id}>" class="delete-me btn btn-danger btn-sm mr-1 me-md-2">Delete</button>
+                <button data-id="<${item._id}>" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                <button data-id="<${item._id}>" class="delete-me btn btn-danger btn-sm mr-1">Delete</button>
             </div>
         </li>`;
 }
@@ -20,13 +20,34 @@ document
 
     axios
     .post("/create-item", { reja: createField.value })
-    .then(response => {
+    .then((response) => {
         document.getElementById("item-list")
         .insertAdjacentHTML("beforeend", itemTemplate(response.data));
         createField.value = "";
         createField.focus();
     })
-    .catch(err => {
+    .catch((err) => {
         console.log("Please try again")
     });
-})
+});
+
+document.addEventListener("click", function (e) {
+    // delete oper
+    console.log(e.target);
+    if(e.target.classList.contains("delete-me")) {
+        if(confirm("Do you want to delete this item?")) {
+           axios
+           .post("/delete-item", { id: e.target.getAttribute("data-id")})
+           .then((response) => {
+            console.log(response.data);
+            e.target.parentElement.parentElement.remove();
+           })
+           .catch((err) => {
+            console.log("Please try again!!!")
+           });
+        }
+    }
+    if(e.target.classList.contains("edit-me")) {
+        alert("You clicked the edit button!");
+    }
+});
