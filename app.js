e08c123ad041f33affd7b7571b0d8d76103729fb
MakhmudSD <makhwork15@gmail.com>
2025-02-27 15:48:14 +0900

    Reja Repository Final Logic Part

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.post("/create-item", (req, res) => {
     })
 });
 
+app.post("/delete-all", (req, res) => {
+    if(req.body.delete_all) {
+        db.collection("plans").deleteMany(function() {
+            res.json( {state: "all plans are deleted"});
+        });
+    }
+});
+
 app.post("/delete-item", (req, res) => {
     const id = req.body.id;
     db.collection("plans")
@@ -29,6 +37,18 @@ app.post("/delete-item", (req, res) => {
     function (err, data) {
             res.json({ state: "success" });
         });   
+});
+
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
 });
 
 app.get("/author", (req, res) => {
--- public/browser.js
+++ public/browser.js
@@ -47,7 +47,35 @@ document.addEventListener("click", function (e) {
            });
         }
     }
-    if(e.target.classList.contains("edit-me")) {
-        alert("You clicked the edit button!");
+    // edit oper
+    if (e.target.classList.contains("edit-me")) {
+      let userInput = prompt(
+        "Edit your plan",
+        e.target.parentElement.parentElement.querySelector(".item-text")
+          .innerHTML
+      );
+      if (userInput) {
+        axios
+          .post("/edit-item", {
+            id: e.target.getAttribute("data-id"),
+            new_input: userInput,
+          })
+          .then((response) => {
+            console.log(response.data);
+            e.target.parentElement.parentElement.querySelector(
+              ".item-text"
+            ).innerHTML = userInput;
+          })
+          .catch((err) => {
+            console.log("Please try again!")
+          });
+      }
     }
+});
+
+document.getElementById("clean-all").addEventListener("click", function() {
+    axios.post("/delete-all", {delete_all: true}).then((response) => {
+        alert(response.data.state);
+        document.location.reload();
+    });
 });
