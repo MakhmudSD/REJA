4d98dec14478d4c31aad3ef16f339f3b70d67e81
MakhmudSD <makhwork15@gmail.com>
2025-03-01 23:34:40 +0900

    Practice done on app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 console.log("Let's start the project");
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// MongoDB connected
 const db = require("./server").db();
 const mongodb = require("mongodb");
 
@@ -14,58 +13,51 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 app.post("/create-item", (req, res) => {
-    console.log("user entered /create-item")
-    console.log(req.body);
-    const new_reja = req.body.reja;
-    db.collection("plans").insertOne({ reja : new_reja}, (err, data) => {
-        res.json(data.ops[0]);
-    })
+  console.log("user entered /create-item");
+  console.log(req.body);
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    res.json(data.ops[0]);
+  });
 });
 
 app.post("/delete-all", (req, res) => {
-    if(req.body.delete_all) {
-        db.collection("plans").deleteMany(function() {
-            res.json( {state: "all plans are deleted"});
-        });
-    }
+  console.log("user entered /delete-all");
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "All items are deleted" });
+    });
+  };
 });
 
 app.post("/delete-item", (req, res) => {
-    const id = req.body.id;
-    db.collection("plans")
-    .deleteOne({_id: new mongodb.ObjectId(id)}, 
-    function (err, data) {
-            res.json({ state: "success" });
-        });   
-});
+    console.log("user entered /delete-item")
+    const id = req.body.id
+    db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data) {
+        res.json({state: "success"});
+    });
+})
 
 app.post("/edit-item", (req, res) => {
-  const data = req.body;
-  console.log(data);
-  db.collection("plans").findOneAndUpdate(
-    { _id: new mongodb.ObjectId(data.id) },
-    { $set: { reja: data.new_input } },
-    function (err, data) {
-      res.json({ state: "success" });
-    }
-  );
-});
-
-app.get("/author", (req, res) => {
-    res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-    console.log("user entered /")
+    console.log("user entered /edit-item")
+    const data = req.body;
     db.collection("plans")
-    .find()
-    .toArray((err, data) => {
+    .findOneAndUpdate(
+        { _id: new mongodb.ObjectId(data.id) },
+        { $set: { reja: data.new_input } },
+        function (err, data) {
+        res.json({state: "success"});
+    });
+});
+
+app.get("/", (req, res) => {
+    console.log("user entered /")
+    db.collection("plans").find().toArray(function (err, data) {
         if(err) {
-            console.log("Something went wrong");
             res.end(err);
-        }else {
-            res.render("reja", { items: data});
-        }
+        } else {
+            res.render("reja", {items:data})
+        };
     });
 });
 
